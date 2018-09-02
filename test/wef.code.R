ca4efbaa58ac5c88e87536d3434baf818a5c92af
Reg Onglao <mrpsonglao@gmail.com>
2018-09-02 16:38:28 +0800

    update test code

---
+++ test/wef.code.R
@@ -0,0 +1,14 @@
+
+library(data360r)
+
+# search
+search_360("Independent judiciary")
+
+# get data set
+df <- get_data360(dataset_id = 24741)
+
+# R version: 3.4.2 (2017-09-28)
+
+# RStudio version: 1.1.383
+
+
